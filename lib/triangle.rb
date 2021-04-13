8d459e2b8a6fa70309ced92b52ead2c115847508
Erica Little <eteney90@gmail.com>
2021-04-13 21:53:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,6 +8,18 @@ class Triangle
     @c = c
   end
 
+  def kind
+  if a<=0 || b<=0 || c<=0 || a+b<=c || a+c<=b || b+c<=a
+    raise TriangleError
+  elsif a==b&&b==c&&a==c
+    :equilateral
+  elsif a==b||b==c||a==c
+    :isosceles
+  elsif a!=b&&b!=c&&a!=c
+    :scalene
+  end
+end
+
 
   class TriangleError < StandardError
 
