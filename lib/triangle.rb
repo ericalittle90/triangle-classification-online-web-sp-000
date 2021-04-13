326273d3882fcf65a360a1033e37dd0ea656112e
Erica Little <eteney90@gmail.com>
2021-04-13 21:54:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,20 +9,18 @@ class Triangle
   end
 
   def kind
-  if a<=0 || b<=0 || c<=0 || a+b<=c || a+c<=b || b+c<=a
-    raise TriangleError
-  elsif a==b&&b==c&&a==c
-    :equilateral
-  elsif a==b||b==c||a==c
-    :isosceles
-  elsif a!=b&&b!=c&&a!=c
-    :scalene
+    if a<=0 || b<=0 || c<=0 || a+b<=c || a+c<=b || b+c<=a
+      raise TriangleError
+    elsif a==b&&b==c&&a==c
+      :equilateral
+    elsif a==b||b==c||a==c
+      :isosceles
+    elsif a!=b&&b!=c&&a!=c
+      :scalene
+    end
   end
-end
-
 
   class TriangleError < StandardError
 
   end
-
 end
